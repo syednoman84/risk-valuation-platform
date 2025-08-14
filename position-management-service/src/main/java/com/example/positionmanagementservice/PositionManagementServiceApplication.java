ba96cd670fbfff7619bc66da0569f7c27d6b55ff
Noman Ahmed <noman.ahmed@amount.com>
2025-08-14 13:25:36 -0400

    first commit

---
+++ position-management-service/src/main/java/com/example/positionmanagementservice/PositionManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.positionmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PositionManagementServiceApplication.class, args);
+    }
+
+}
---
+++ position-management-service/src/test/java/com/example/positionmanagementservice/PositionManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.positionmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositionManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
